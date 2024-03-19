d575a54713e1c1a308e9c74cfba93a3e4a796a4b
Yury Shibitov <y.shibitov@paynocchio.com>
2024-03-19 11:55:09 +0300

    feat: topup mini form in widget

--- views/paynocchio-cart-wallet-widget.php
+++ views/paynocchio-cart-wallet-widget.php
@@ -41,21 +41,33 @@ if (!defined('ABSPATH')) {
             </div>
         </div>
 
-       <!-- <div class="topup_mini_form cfps-hidden cfps-absolute cfps-top-[110%] cfps-left-0 cfps-w-full">
-            <div class="cfps-p-4 cfps-bg-white cfps-drop-shadow cfps-flex cfps-flex-row cfps-items-center cfps-gap-x-2 cfps-rounded-lg">
-                <p class="cfps-text-xl cfps-font-semibold">$</p>
-                <input type="number" class="cfps-border-0 cfps-rounded-lg cfps-text-xl cfps-font-semibold cfps-w-full" value="0" id="top_up_amount" />
+        <div class="topup_mini_form cfps-hidden cfps-absolute cfps-top-[110%] cfps-left-0 cfps-w-full">
+            <div class="cfps-p-4 cfps-bg-white cfps-drop-shadow cfps-flex cfps-flex-row cfps-items-center cfps-gap-x-2 cfps-rounded-lg cfps-justify-between">
+                <div class="cfps-flex cfps-flex-row">
+                    <p class="cfps-text-xl cfps-font-semibold cfps-whitespace-nowrap"> Add $</p>
+                     <input type="number" class="cfps-border-0 cfps-p-0 cfps-rounded-lg cfps-text-xl cfps-font-semibold cfps-w-[100px]"
+                       placeholder="0" value="" id="top_up_amount_mini_form" />
+                </div>
                 <button id="top_up_mini_form_button"
                         type="button"
-                        class="cfps-border-0 cfps-rounded-lg cfps-p-2 cfps-bg-blue-500 cfps-text-white cfps-whitespace-nowrap">
+                        class="cfps-border-0 cfps-rounded-lg cfps-p-2 cfps-bg-blue-500 cfps-text-white cfps-flex cfps-flex-row">
                     Top up
                     <svg class="cfps-spinner cfps-hidden cfps-animate-spin cfps-ml-4 cfps-h-5 cfps-w-5 cfps-text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="cfps-opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="cfps-opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
+                    <svg class="cfps-check cfps-hidden cfps-ml-4 cfps-h-5 cfps-w-5 cfps-text-white cfps-fill-white" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"></path>
+                    </svg>
+                    <svg class="cfps-cross cfps-hidden cfps-ml-4 cfps-h-5 cfps-w-5 cfps-text-white cfps-fill-white" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                        <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M256 32c14.2 0 27.3 7.5 34.5 19.8l216 368c7.3 12.4 7.3 27.7 .2 40.1S486.3 480 472 480H40c-14.3 0-27.6-7.7-34.7-20.1s-7-27.8 .2-40.1l216-368C228.7 39.5 241.8 32 256 32zm0 128c-13.3 0-24 10.7-24 24V296c0 13.3 10.7 24 24 24s24-10.7 24-24V184c0-13.3-10.7-24-24-24zm32 224a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path>
+                    </svg>
                 </button>
-                <?php /*wp_nonce_field( 'paynocchio_ajax_top_up', 'ajax-top-up-nonce' ); */?>
+                <?php wp_nonce_field( 'paynocchio_ajax_top_up', 'ajax-top-up-nonce-mini-form' ); ?>
             </div>
-        </div>-->
+
+        </div>
     <?php } ?>
 </div>
